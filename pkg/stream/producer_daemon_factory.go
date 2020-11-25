6ff5119fc1506d0ba517e67af349675c4d21896e
Jan K <jankamieth@gmail.com>
2020-11-25 11:52:34 +0100

    wip

--- pkg/stream/producer_daemon_factory.go
+++ pkg/stream/producer_daemon_factory.go
@@ -1,14 +1,15 @@
 package stream
 
 import (
+	"context"
 	"fmt"
 	"github.com/applike/gosoline/pkg/cfg"
 	"github.com/applike/gosoline/pkg/kernel"
 	"github.com/applike/gosoline/pkg/mon"
 )
 
-func ProducerDaemonFactory(config cfg.Config, logger mon.Logger) (map[string]kernel.Module, error) {
-	modules := map[string]kernel.Module{}
+func ProducerDaemonFactory(config cfg.Config, logger mon.Logger) (map[string]kernel.ModuleFactory, error) {
+	modules := map[string]kernel.ModuleFactory{}
 	producerDaemonSettings := readAllProducerDaemonSettings(config)
 
 	for name, settings := range producerDaemonSettings {
@@ -17,7 +18,9 @@ func ProducerDaemonFactory(config cfg.Config, logger mon.Logger) (map[string]ker
 		}
 
 		moduleName := fmt.Sprintf("producer-daemon-%s", name)
-		modules[moduleName] = ProvideProducerDaemon(config, logger, name)
+		modules[moduleName] = func(ctx context.Context, config cfg.Config, logger mon.Logger) (kernel.Module, error) {
+			return ProvideProducerDaemon(config, logger, name), nil
+		}
 	}
 
 	return modules, nil
